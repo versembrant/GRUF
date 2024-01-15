4cbfc79a7d62786fbbd1c6bde105249dc6e891c3
ffont <frederic.font@upf.edu>
2024-01-15 11:50:24 +0100

    Remove unneded file

--- static/src/js/utils.js
+++ static/src/js/utils.js
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import { createElement, useState, useEffect, StrictMode } from "react";
-import { FloatParameterDefaultWidget, EnumParameterDefaultWidget, TextParameterDefaultWidget, StepsParameterDefaultWidget, GridParameterDefaultWidget } from './components/defaultUIParameterWidgets';
 
 // Make sure numeric value is within min/max boundaries
 export const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
