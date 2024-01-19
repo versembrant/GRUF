8287175548eb19bb7adca9100f541ce9c8d407fa
ffont <frederic.font@upf.edu>
2024-01-19 10:47:12 +0100

    Highlight client user

--- static/src/js/components/sessionConnectedUsers.jsx
+++ static/src/js/components/sessionConnectedUsers.jsx
@@ -1,11 +1,12 @@
 import { subscribeToStoreChanges } from "../utils";
 import { getCurrentSession } from "../sessionManager";
+import { getSocketID } from "../serverComs";
 
 export const SessionConnectedUsers = () => {
     subscribeToStoreChanges(getCurrentSession());
     return (
         <div>
-            Connected users: {getCurrentSession().getConnectedUsers().join(', ')}
+            Connected users: {getCurrentSession().getConnectedUsers().map(sessionID => <span style={sessionID == getSocketID() ? {color:'green', marginRight:'8px'}:{marginRight:'8px'}}>{sessionID}</span>)}
         </div>
     )
 };
