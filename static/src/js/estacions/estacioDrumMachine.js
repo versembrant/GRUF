bc79588e1bd6e148b510731940d072461cf3b51b
ffont <frederic.font@upf.edu>
2024-02-21 11:01:38 +0100

    Implementa swing individual a drum machine

--- static/src/js/estacions/estacioDrumMachine.js
+++ static/src/js/estacions/estacioDrumMachine.js
@@ -1,6 +1,7 @@
 import * as Tone from 'tone'
 import { EstacioBase } from "../sessionManager";
-import { indexOfArray } from '../utils';
+import { indexOfArray, clamp, necessitaSwing } from '../utils';
+import { getAudioGraphInstance } from '../audioEngine';
 
 export class EstacioDrumMachine extends EstacioBase {
     
@@ -8,11 +9,14 @@ export class EstacioDrumMachine extends EstacioBase {
     versio = '0.1'
     parametersDescription = {
         sound1URL: {type: 'text', label: 'Clap', initial: 'https://cdn.freesound.org/previews/125/125591_4948-hq.mp3'}, // Clap
+        swing1: {type: 'float', label: 'Swing1', min: 0, max: 1, initial: 0},
         sound2URL: {type: 'text', label: 'HiHat', initial: 'https://cdn.freesound.org/previews/75/75840_260058-hq.mp3'}, // Hat
+        swing2: {type: 'float', label: 'Swing2', min: 0, max: 1, initial: 0},
         sound3URL: {type: 'text', label: 'Snare', initial: 'https://cdn.freesound.org/previews/693/693151_14904072-hq.mp3'}, // Snare
+        swing3: {type: 'float', label: 'Swing3', min: 0, max: 1, initial: 0},
         sound4URL: {type: 'text', label: 'Kick', initial: 'https://cdn.freesound.org/previews/274/274775_4965320-hq.mp3'}, // Kick
-        pattern: {type: 'grid', label:'Pattern', numRows: 4, numCols: 16, initial:[]},
-        //swing1: {type: 'float', label: 'Swing1', min: 0, max: 1, initial: 0}
+        swing4: {type: 'float', label: 'Swing4', min: 0, max: 1, initial: 0},
+        pattern: {type: 'grid', label:'Pattern', numRows: 4, numCols: 32, initial:[]},
     }
     noteURLsNumbers = {}
 
@@ -61,12 +65,9 @@ export class EstacioDrumMachine extends EstacioBase {
 
     playSoundFromUrl(url, time) {
         const note = this.getNoteNumber(url)
-        //let swing = this.getParameterValue ('swing1')
         if (note !== undefined){
             this.audioNodes.sampler.triggerAttack(note, time);
         }
-        //this.audioNodes.sampler.Transport.swing = swing;
-
     }
 
     onSequencerTick(currentMainSequencerStep, time) {
@@ -77,17 +78,30 @@ export class EstacioDrumMachine extends EstacioBase {
         const shouldPlaySound2 = indexOfArray(pattern, [1, currentStep]) > -1;
         const shouldPlaySound3 = indexOfArray(pattern, [2, currentStep]) > -1;
         const shouldPlaySound4 = indexOfArray(pattern, [3, currentStep]) > -1;
+        let modificadorTempsSwingGeneral = 0.0;
+        let modificatorTempsSwing1 = 0.0;
+        let modificatorTempsSwing2 = 0.0;
+        let modificatorTempsSwing3 = 0.0;
+        let modificatorTempsSwing4 = 0.0;
+        const tempsBeat = 60.0 / getAudioGraphInstance().getBpm() / 4.0;
+        if (necessitaSwing(currentStep)) {
+            modificadorTempsSwingGeneral = tempsBeat * getAudioGraphInstance().getSwing();
+            modificatorTempsSwing1 = tempsBeat * this.getParameterValue('swing1', this.currentPreset);
+            modificatorTempsSwing2 = tempsBeat * this.getParameterValue('swing2', this.currentPreset);
+            modificatorTempsSwing3 = tempsBeat * this.getParameterValue('swing3', this.currentPreset);
+            modificatorTempsSwing4 = tempsBeat * this.getParameterValue('swing4', this.currentPreset);
+        }
         if (shouldPlaySound1) {
-            this.playSoundFromUrl(this.getParameterValue('sound1URL', this.currentPreset), time)
+            this.playSoundFromUrl(this.getParameterValue('sound1URL', this.currentPreset), time + clamp(modificadorTempsSwingGeneral + modificatorTempsSwing1, 0, 1))
         }
         if (shouldPlaySound2) {
-            this.playSoundFromUrl(this.getParameterValue('sound2URL', this.currentPreset), time)
+            this.playSoundFromUrl(this.getParameterValue('sound2URL', this.currentPreset), time + clamp(modificadorTempsSwingGeneral + modificatorTempsSwing2, 0, 1))
         }
         if (shouldPlaySound3) {
-            this.playSoundFromUrl(this.getParameterValue('sound3URL', this.currentPreset), time)
+            this.playSoundFromUrl(this.getParameterValue('sound3URL', this.currentPreset), time + clamp(modificadorTempsSwingGeneral + modificatorTempsSwing3, 0, 1))
         }
         if (shouldPlaySound4) {
-            this.playSoundFromUrl(this.getParameterValue('sound4URL', this.currentPreset), time)
+            this.playSoundFromUrl(this.getParameterValue('sound4URL', this.currentPreset), time + clamp(modificadorTempsSwingGeneral + modificatorTempsSwing4, 0, 1))
         }
     }
 }
