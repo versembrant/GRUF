67144dcda8ae9be0da89603215f97f886a002a8f
ffont <frederic.font@upf.edu>
2024-02-28 00:32:13 +0100

    Afegeix suport per desplegaments al servidor remot en mode test

--- fabfile.py
+++ fabfile.py
@@ -13,6 +13,7 @@ docker-compose run --rm server fab deploy-nginx-conf
 """
 
 code_dir = "/home/versembrant/versembrant-bruixit/"
+code_dir_test = "/home/versembrant/test/versembrant-bruixit/"
 host = "versembrant@cl2024011711001.dnssw.net"
 host_root = "root@cl2024011711001.dnssw.net"
 pem_file = "/ssh/id_rsa"
@@ -30,48 +31,62 @@ def print_banner(messages):
 
 
 @task
-def deploy(ctx):
-    messages = ["Deploying...", "Host: %s" % host, "Using pem: %s" % pem_file]
+def deploy(ctx, branch="main", test=False):
+    messages = ["Deploying...", "Host: %s" % host, "Using pem: %s" % pem_file, "Test: %s" % test, "Branch: %s" % branch]
     print_banner(messages)
 
     with Connection(host=host, connect_kwargs={"key_filename": pem_file}) as c:
-        with c.cd(code_dir):
+        with c.cd(code_dir if not test else code_dir_test):
             # Checkout code
-            c.run("git pull")  # farà pull al branch que hi hagi actiu
+            c.run(f"git fetch")
+            c.run(f"git checkout -f {branch}")
+            c.run("git pull")
+
+            if test:
+                # Copy the .env file for test deployment
+                c.put("deploy/.env_test", code_dir_test + '.env')
+
+            compose_file = "docker-compose.prod.yml" if not test else "docker-compose.test.yml"
+            server_service_name = "server" if not test else "server-test"
+            tmp_dir = 'tmp' if not test else 'tmp-test'
 
             # Build docker image
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml build")
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} build")
 
             # Install and build static files
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml run --rm server yarn install")
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml run --rm server yarn build")
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} run --rm {server_service_name} yarn install")
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} run --rm {server_service_name} yarn build")
 
             # Restart
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml stop")
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml up -d --remove-orphans")
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} stop")
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} up -d")
 
 
 @task
-def stop(ctx):
-    messages = ["Stopping...", "Host: %s" % host, "Using pem: %s" % pem_file]
+def stop(ctx, test=False):
+    messages = ["Stopping...", "Host: %s" % host, "Using pem: %s" % pem_file, "Test: %s" % test]
     print_banner(messages)
 
     with Connection(host=host, connect_kwargs={"key_filename": pem_file}) as c:
-        with c.cd(code_dir):
+        with c.cd(code_dir if not test else code_dir_test):
             # Stop
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml stop")
+            compose_file = "docker-compose.prod.yml" if not test else "docker-compose.test.yml"
+            tmp_dir = 'tmp' if not test else 'tmp-test'
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} stop")
 
 
 @task
-def restart(ctx):
-    messages = ["Stopping...", "Host: %s" % host, "Using pem: %s" % pem_file]
+def restart(ctx, test=False):
+    messages = ["Stopping...", "Host: %s" % host, "Using pem: %s" % pem_file, "Test: %s" % test]
     print_banner(messages)
 
     with Connection(host=host, connect_kwargs={"key_filename": pem_file}) as c:
-        with c.cd(code_dir):
+        with c.cd(code_dir if not test else code_dir_test):
             # Restart
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml stop")
-            c.run("TMPDIR=$HOME/tmp docker compose -f docker-compose.prod.yml up -d --remove-orphans")
+            compose_file = "docker-compose.prod.yml" if not test else "docker-compose.test.yml"
+            tmp_dir = 'tmp' if not test else 'tmp-test'
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} stop")
+            c.run(f"TMPDIR=$HOME/{tmp_dir} docker compose -f {compose_file} up -d")
 
 
 @task
