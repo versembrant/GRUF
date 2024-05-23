4d8071a48bc42958a58b705780b3b66bfa1bdc4d
Gonzalo Bañeres <gonzalo.baneres@gmail.com>
2024-05-23 09:50:40 +0200

    Afegeix fxNodes a estacioSynth

--- static/src/js/estacions/estacioSynth.js
+++ static/src/js/estacions/estacioSynth.js
@@ -17,24 +17,101 @@ export class EstacioSynth extends EstacioBase {
         lpf: {type: 'float', label: 'LPF', min: 500, max: 15000, initial: 15000, logarithmic: true},
         hpf: {type: 'float', label: 'HPF', min: 20, max: 6000, initial: 20, logarithmic: true},
         notes: {type: 'grid', label:'Notes', numRows: 8, numCols: 16, initial:[]},
-        chorusSend:{type: 'float', label: 'Chorus Send', min: -60, max: 6, initial: -60},
         reverbSend:{type: 'float', label: 'Reverb Send', min: -60, max: 6, initial: -60},
         delaySend:{type: 'float', label: 'Delay Send', min: -60, max: 6, initial: -60},
+        driveSend:{type: 'float', label: 'Drive Send', min: -60, max: 6, initial: -60},
+        eq3Send: {type: 'float', label: 'EQ3 Send', min: -60, max: 6, initial: -60},
+        reverbWet: {type: 'float', label:'Reverb Wet', min: 0.0, max: 1.0, initial: 1.0},
+        reverbDecay: {type: 'float', label:'Reverb Decay', min: 0.1, max: 15, initial: 1.0},
+        delayWet: {type: 'float', label:'Delay Wet', min: 0.0, max: 1.0, initial: 1.0},
+        delayFeedback:{type: 'float', label:'Delay Feedback', min: 0.0, max: 1.0, initial: 0.5},
+        delayTime:{type: 'enum', label:'Delay Time', options: ['1/4', '1/8', '1/16','1/8T', '1/16T'], initial: '1/8'},
+        drive:{type: 'float', label:'Drive', min: 0.0, max: 1.0, initial: 1.0},
+        low:{type: 'float', label:'Low', min: -12, max: 12, initial: 0.0},
+        mid:{type: 'float', label:'Mid', min: -12, max: 12, initial: 0.0},
+        high:{type: 'float', label:'High', min: -12, max: 12, initial: 0.0},
+    }
+
+    updateEffectParameter(effectName, effectKey, paramName, preset){
+        if (paramName === 'delayTime'){
+            const paramValue = this.getDelayTimeValue(this.getParameterValue(paramName, preset))
+            if (this.audioNodes.effects[effectName]){
+                this.audioNodes.effects[effectName].set({[effectKey]: paramValue});
+            }
+        }
+        else {
+            const paramValue = this.getParameterValue(paramName, preset);
+            if (this.audioNodes.effects[effectName]){
+                this.audioNodes.effects[effectName].set({[effectKey]: paramValue});
+            }
+        }
+    }
+    getDelayTimeValue(delayTime){
+        if      (delayTime === '1/4') 
+            return 60/ (1*(getAudioGraphInstance().getBpm()));
+        else if (delayTime === '1/8') 
+            return 60/ (2*(getAudioGraphInstance().getBpm()));
+        else if (delayTime === '1/16') 
+            return 60/ (4*(getAudioGraphInstance().getBpm()));
+        else if (delayTime === '1/8T') 
+            return 60/ (3*(getAudioGraphInstance().getBpm()));
+        else if (delayTime === '1/16T') 
+            return 60/ (6*(getAudioGraphInstance().getBpm()));
     }
 
     buildEstacioAudioGraph(estacioMasterChannel) {
         // Creem els nodes del graph i els guardem
-        const lpf = new Tone.Filter(500, "lowpass").connect(estacioMasterChannel);
-        const hpf = new Tone.Filter(6000, "highpass").connect(estacioMasterChannel);
+        const dryGainNode = new Tone.Channel().connect(estacioMasterChannel);
+        const lpf = new Tone.Filter(500, "lowpass").connect(dryGainNode);
+        const hpf = new Tone.Filter(6000, "highpass").connect(dryGainNode);
         const synth = new Tone.PolySynth(Tone.Synth).connect(lpf).connect(hpf);
+        const effects = {
+            reverb: new Tone.Reverb({
+                decay: 0.5,
+                wet: 0,
+            }),
+            delay: new Tone.FeedbackDelay({
+                wet: 1,
+                feedback: 0.5,
+                delayTime: this.getDelayTimeValue('1/4'),
+            }),
+            drive: new Tone.Distortion({
+                distortion: 0,
+            }),
+            eq3: new Tone.EQ3(),
+        }
+        const effectsChannels = {
+            reverbChannel: new Tone.Channel(),
+            delayChannel: new Tone.Channel(),
+            driveChannel: new Tone.Channel(),
+            eq3Channel: new Tone.Channel(),
+        }
+        //Connectem els efectes a la sortida d'àudio
+        Object.values(effects).forEach(effect => {
+            effect.connect(estacioMasterChannel);
+        }); 
+        //Connectem els canals d'efectes al seu respectiu efecte
+        Object.keys(effectsChannels).forEach((key, index) => {
+            const effectKey = Object.keys(effects)[index];
+            effectsChannels[key].connect(effects[effectKey]);
+        });
+
+        Object.keys(effectsChannels).forEach(key => {
+            const busName = key.replace('Channel', 'Synth'); // exemple: reverbChannel -> reverbSynth
+            effectsChannels[key].receive(busName);
+        });
+
+
         synth.set({maxPolyphony: 16});
         this.audioNodes = {
             synth: synth,
             lpf: lpf,
             hpf: hpf,
-            sendReverbGainNode: estacioMasterChannel.send("reverb", -100),
-            sendChorusGainNode: estacioMasterChannel.send("chorus", -100),
-            sendDelayGainNode: estacioMasterChannel.send("delay", -100),
+            effects: effects,
+            sendReverbGainNode: dryGainNode.send("reverbSynth", -100),
+            sendDelayGainNode: dryGainNode.send("delaySynth", -100),
+            sendDriveGainNode: dryGainNode.send("driveSynth", -100),
+            sendEq3GainNode: dryGainNode.send('eq3Synth', -100),
         };
     }
 
@@ -54,8 +131,14 @@ export class EstacioSynth extends EstacioBase {
         this.audioNodes.lpf.frequency.rampTo(this.getParameterValue('lpf', preset),0.01);
         this.audioNodes.hpf.frequency.rampTo(this.getParameterValue('hpf', preset),0.01);
         this.audioNodes.sendReverbGainNode.gain.value = this.getParameterValue('reverbSend',preset);
-        this.audioNodes.sendChorusGainNode.gain.value = this.getParameterValue('chorusSend',preset);
         this.audioNodes.sendDelayGainNode.gain.value = this.getParameterValue('delaySend',preset);
+        this.audioNodes.sendDriveGainNode.gain.value = this.getParameterValue('driveSend',preset);
+        this.updateEffectParameter('reverb','wet','reverbWet', preset);
+        this.updateEffectParameter('reverb','decay','reverbDecay', preset);
+        this.updateEffectParameter('delay','wet','delayWet', preset);
+        this.updateEffectParameter('delay','wet','delayTime', preset);
+        this.updateEffectParameter('delay','feedback','delayFeedback', preset);
+        this.updateEffectParameter('drive','distortion','drive', preset);
     }
 
     updateAudioGraphParameter(nomParametre, preset) {
