0444d0699cb22fc4d86fb0edb2fa87d2e170c672
Gonzalo Bañeres <gonzalo.baneres@gmail.com>
2024-07-12 15:55:58 +0200

    Incorpora samplerUI i modifica l'estil dels pads

--- static/src/js/components/estacioSampler.jsx
+++ static/src/js/components/estacioSampler.jsx
@@ -3,14 +3,14 @@ import { subscribeToStoreChanges } from "../utils";
 import { getCurrentSession } from "../sessionManager";
 import { getAudioGraphInstance } from '../audioEngine';
 import { indexOfArrayMatchingObject, real2Norm, norm2Real, hasPatronsPredefinits, getNomPatroOCap, getPatroPredefinitAmbNom} from "../utils";
-import { GrufKnobGran, GrufKnobPetit, GrufLabel, GrufEnum2Columns, GrufReverbTime } from "./widgets";
+import { GrufKnobGran, GrufKnobPetit, GrufLabel, GrufEnum2Columns, GrufReverbTime, GrufPad, PadGrid } from "./widgets";
 
 
-export const EstacioPianoUI = ({estacio}) => {
+export const EstacioSamplerUI = ({estacio}) => {
     subscribeToStoreChanges(estacio);  // Subscriu als canvis de paràmetres de la pròpia estació
     subscribeToStoreChanges(getAudioGraphInstance());  // Subscriu als canvis de l'audio graph per actualizar current step del sequencer principal
 
-    return (<div key={estacio.nom} className="estacio estacio-piano">
+    return (<div key={estacio.nom} className="estacio estacio-sampler">
         <div className="estacio-main">
             <GrufLabel text="EQ" top="7%" left="6%" />
             <GrufKnobGran estacio={estacio} parameterName="fxLow" top="12%" left="5.8%" />
@@ -18,11 +18,10 @@ export const EstacioPianoUI = ({estacio}) => {
             <GrufKnobGran estacio={estacio} parameterName="fxHigh" top="12%" left="25%" />
             <GrufLabel text="Reverb" top="7.3%" left="37%" />
             <GrufLabel text="Durada" top="11.5%" left="36.7%" />
-            <GrufEnum2Columns estacio={estacio} parameterName="fxDelayTime" top="50%" left="50%" />
             <GrufReverbTime estacio={estacio} parameterName="fxReverbDecay" top="15.5%" left="36.7%" />
             <GrufKnobGran estacio={estacio} parameterName="fxReverbWet" top="6.7%" left="54.8%" />
             <GrufLabel text="Timbre" top="7.2%" left="68.2%" />
-            <GrufLabel text="Hola" top="50.2%" left="68.2%" />
+            <PadGrid top="60%" left="40%" />
         </div>
         <div className="estacio-bottom-bar">
             {estacio.tipus}
