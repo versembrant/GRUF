d73d3e6f3efe1f5b7e6c5dbdb6aac03453126318
ffont <frederic.font@gmail.com>
2024-07-23 23:52:05 +0200

    Formata frontpage

---
+++ static/src/js/components/frontpage.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const Frontpage = () => {
+    
+    return(
+        <div className="frontpage">
+            <div className="navbar">
+                <ul>
+                    <li><a href={appPrefix + "/"}>Versembrant</a></li>
+                    <li><a href={appPrefix + "/"}>Turorials</a></li>
+                </ul>
+            </div>
+            <div className="hero">
+                <h1>Grufa sense parar!</h1>
+                <h2>Crea la teva música sol o en comunitat amb programari llirue i gratuït</h2>
+                <ul class="buttons">
+                    <li><a href={appPrefix + "/nova_sessio"}>Nou GRUF</a></li>
+                    <li><a href={appPrefix + "/connecta"}>Connecta't a un GRUF</a></li>
+                </ul>
+            </div>
+
+            <div className="footer">
+                <ul>
+                    <li>Un projecte de <a href="https://versembrant.cat/"><img height="25px" src="https://versembrant.cat/wp-content/uploads/2019/09/cropped-LOGO-COMPLET-Versembrant-Color-RGB-e1604594991611.png"/></a></li>
+                    <li>Amb el support de <a href="https://empresa.gencat.cat/ca/inici/index.html"><img height="25px" src={appPrefix + "/static/src/img/logo_gene.svg"}/></a></li>
+                    <li><a href={appPrefix + "/"}>Avís legal</a></li>
+                </ul>
+            </div>
+            
+        </div>
+    )
+};
