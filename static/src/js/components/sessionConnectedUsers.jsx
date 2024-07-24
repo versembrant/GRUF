e0c003999373c9f317c5d969aee28dbaac0b5558
ffont <frederic.font@gmail.com>
2024-07-24 16:01:11 +0200

    Afegir logo gruf

--- static/src/js/components/sessionConnectedUsers.jsx
+++ static/src/js/components/sessionConnectedUsers.jsx
@@ -5,7 +5,7 @@ import { getSocketID } from "../serverComs";
 export const SessionConnectedUsers = () => {
     subscribeToStoreChanges(getCurrentSession());
     return (
-        <div className="ellipsis" style={{"max-width": "700px"}}>
+        <div className="ellipsis" style={{"maxWidth": "700px"}}>
             Usuaris connectats ({getCurrentSession().getConnectedUsers().length}): {getCurrentSession().getConnectedUsers().map(sessionID => <span key={sessionID} style={sessionID == getSocketID() ? {color:'green', marginRight:'8px'}:{marginRight:'8px'}}>{sessionID}</span>)}
         </div>
     )
