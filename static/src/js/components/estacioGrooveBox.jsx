14b448553b35ad8ce29acf1cdcc259bbca9a97b3
Gonzalo Bañeres <gonzalo.baneres@gmail.com>
2024-07-30 10:38:16 +0200

    Arregla GrufOnOffButton

--- static/src/js/components/estacioGrooveBox.jsx
+++ static/src/js/components/estacioGrooveBox.jsx
@@ -9,24 +9,24 @@ export const EstacioGrooveBoxUI = ({estacio, setEstacioSelected}) => {
 
     return (<div key={estacio.nom} className="estacio estacio-grooveBox">
         <div className="estacio-main">
-            <GrufKnobGran estacio={estacio} parameterName="volume1" top="7%" left="6.3%" label="Vol" />
-            <GrufKnobGran estacio={estacio} parameterName="swing1" top="7%" left="17.8%" label = 'Swing' />
-            <GrufKnobGran estacio={estacio} parameterName="swing2" top="7%" left="29.0%" label = 'Tempo' />
-            <GrufBpmCounter estacio={estacio} top="8.5%" left="39.6%" />
-            <GrufLabel text="bpm" top="17.8%" left="41%" />
+            <GrufKnobGran estacio={estacio} parameterName="volume" top="8%" left="6.3%" label="Vol" />
+            <GrufKnobGran estacio={estacio} parameterName="swing1" top="8%" left="17.8%" label = 'Swing' />
+            <GrufKnobGran estacio={estacio} parameterName="swing2" top="8%" left="29.0%" label = 'Tempo' />
+            <GrufBpmCounter estacio={estacio} top="8%" left="39.6%" />
+            <GrufLabel text="bpm" top="17.3%" left="41%" />
             <GrufLabel text="Reverb" top="7.3%" left="51.7%" />
             <GrufLabel text="Durada" top="12.3%" left="51.7%" />
-            <GrufReverbTime estacio={estacio} parameterName="reverbSend2" top="15.0%" left="51.7%" />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="6.6%" left="72.8%" label = 'Send' />
-            <GrufLabel text="Tria instrument" top="6.1%" left="81.2%" />
-            <GrufLabel text="Delay" top="14%" left="82.5%" />
-            <GrufOnOffButton estacio={estacio} parameterName="reverbSend3" top="19.5%" left="81.7%" />
-            <GrufLabel text="EQ" top="29.5%" left="51.7%" />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="34.5%" left="51.9%" label = 'Low' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="34.5%" left="56.9%" label = 'Mid' /> 
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="34.5%" left="61.9%" label = 'High' />
-            <GrufLabel text="Durada" top="29.5%" left="70.3%" />
-            <GrufEnum2Columns estacio={estacio} parameterName="fxDelayTime" top="50%" left="50%" />
+            <GrufReverbTime estacio={estacio} parameterName="fxReverbDecay" top="15.0%" left="51.7%" />
+            <GrufKnobPetit estacio={estacio} parameterName="fxReverbWet" top="6.6%" left="72.8%" label = 'Send' />
+            <GrufLabel text="Tria instrument" top="6.2%" left="81.2%" />
+            <GrufLabel text="Delay" top="14.1%" left="82.5%" />
+            <GrufOnOffButton estacio={estacio} parameterName="fxDelayWet" top="19.5%" left="81.8%" />
+            <GrufLabel text="EQ" top="29.6%" left="51.7%" />
+            <GrufKnobPetit estacio={estacio} parameterName="fxLow" top="34.5%" left="51.9%" label = 'Low' />
+            <GrufKnobPetit estacio={estacio} parameterName="fxMid" top="34.5%" left="56.9%" label = 'Mid' /> 
+            <GrufKnobPetit estacio={estacio} parameterName="fxHigh" top="34.5%" left="61.9%" label = 'High' />
+            <GrufLabel text="Durada" top="29.6%" left="70.3%" />
+            <GrufEnum2Columns estacio={estacio} parameterName="fxDelayTime" top="34.2%" left="69.4%" />
             {/* <GrufSlider estacio={estacio} parameterName="reverbSend1" top="110px" left="694px" width="225px"/> */}
             <GrufLabelPetit text="Mix" top="38.5%" left="84.2%" />
             <GrufLabelPetit text="Feedback" top="38.5%" left="88.0%" />
@@ -53,15 +53,15 @@ export const EstacioGrooveBoxUI = ({estacio, setEstacioSelected}) => {
             <GrufKnobPetit estacio={estacio} parameterName="reverbSend3" top="61%" left="87.5%" label = 'Reverb' />
             <GrufKnobPetit estacio={estacio} parameterName="swing3" top="61%" left="93%" label = 'Swing' />
 
-            <GrufKnobPetit estacio={estacio} parameterName="atack3" top="72.5%" left="77%" label = 'Attack' />
-            <GrufKnobPetit estacio={estacio} parameterName="release3" top="72.5%" left="82%" label = 'Release' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend3" top="72.5%" left="87.5%" label = 'Reverb' />
-            <GrufKnobPetit estacio={estacio} parameterName="swing3" top="72.5%" left="93%" label = 'Swing' />
+            <GrufKnobPetit estacio={estacio} parameterName="atack2" top="72.5%" left="77%" label = 'Attack' />
+            <GrufKnobPetit estacio={estacio} parameterName="release2" top="72.5%" left="82%" label = 'Release' />
+            <GrufKnobPetit estacio={estacio} parameterName="reverbSend2" top="72.5%" left="87.5%" label = 'Reverb' />
+            <GrufKnobPetit estacio={estacio} parameterName="swing2" top="72.5%" left="93%" label = 'Swing' />
 
-            <GrufKnobPetit estacio={estacio} parameterName="atack4" top="85%" left="77%" label = 'Attack' />
-            <GrufKnobPetit estacio={estacio} parameterName="release4" top="85%" left="82%" label = 'Release' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend4" top="85%" left="87.5%" label = 'Reverb' />
-            <GrufKnobPetit estacio={estacio} parameterName="swing4" top="85%" left="93%" label = 'Swing' />
+            <GrufKnobPetit estacio={estacio} parameterName="atack1" top="85%" left="77%" label = 'Attack' />
+            <GrufKnobPetit estacio={estacio} parameterName="release1" top="85%" left="82%" label = 'Release' />
+            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="85%" left="87.5%" label = 'Reverb' />
+            <GrufKnobPetit estacio={estacio} parameterName="swing1" top="85%" left="93%" label = 'Swing' />
 
                         
             
