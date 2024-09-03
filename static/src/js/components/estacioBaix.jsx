6643dd3fdcd01f3a0cce9584970abbf8f463a7ff
ffont <frederic.font@gmail.com>
2024-09-03 16:59:05 +0200

    Ajusta alguns controls a estacions baix, synth i groovebox

--- static/src/js/components/estacioBaix.jsx
+++ static/src/js/components/estacioBaix.jsx
@@ -1,6 +1,6 @@
 import { subscribeToStoreChanges } from "../utils";
 import { getAudioGraphInstance } from '../audioEngine';
-import { GrufKnobGran, GrufKnobPetit, GrufLabel, GrufEnum2Columns, GrufReverbTime, GrufOnOffButton, GrufButtonNoBorder, GrufSliderVertical, GrufPianoRoll } from "./widgets";
+import { GrufKnobGran, GrufLabelPetit, GrufKnobPetit, GrufLabel, GrufEnum2Columns, GrufReverbTime, GrufOnOffButton, GrufButtonNoBorder, GrufSliderVertical, GrufPianoRoll } from "./widgets";
 
 
 export const EstacioBaixUI = ({estacio, setEstacioSelected}) => {
@@ -10,31 +10,38 @@ export const EstacioBaixUI = ({estacio, setEstacioSelected}) => {
     return (<div key={estacio.nom} className="estacio estacio-synth_bass">
         <div className="estacio-main">
         <GrufKnobPetit estacio={estacio} parameterName="attack" top="35%" left="5%" />
+        <GrufButtonNoBorder text="Canvia estació" top="42px" left="822px" onClick={() => {setEstacioSelected(undefined)}} />
+            <GrufKnobPetit estacio={estacio} parameterName="attack" top="35%" left="5%" />
             <GrufKnobPetit estacio={estacio} parameterName="decay" top="35%" left="10%" />
             <GrufKnobPetit estacio={estacio} parameterName="sustain" top="35%" left="14.7%" />
             <GrufKnobPetit estacio={estacio} parameterName="release" top="35%" left="19.8%" />
             <GrufLabel text="EQ" top="29.3%" left="52%" />
             <GrufKnobGran estacio={estacio} parameterName="harmonicity" top="30%" left="28%" />
-            <GrufKnobGran estacio={estacio} parameterName="portamento" top="7.8%" left="28%" />
+            
             <GrufLabel text="Reverb" top="7.3%" left="52%" />
-            <GrufButtonNoBorder text="Canvia estació" top="42px" left="822px" onClick={() => {setEstacioSelected(undefined)}} />
-            <GrufEnum2Columns estacio={estacio} parameterName="fxDelayTime" top="34%" left="69.4%" />
-            <GrufReverbTime estacio={estacio} parameterName="fxReverbDecay" top="15%" left="51.2%" />
+            <GrufLabel text="Durada" top="12.3%" left="51.7%" />
+            <GrufReverbTime estacio={estacio} parameterName="fxReverbDecay" top="117px" left="51.7%" />
+            <GrufKnobPetit estacio={estacio} parameterName="fxReverbWet" top="6.6%" left="72.8%" label = 'Send' />
+            
             <GrufKnobPetit estacio={estacio} parameterName="fxLow" top="35%" left="51.5%" />
             <GrufKnobPetit estacio={estacio} parameterName="fxMid" top="35%" left="56.4%" />
             <GrufKnobPetit estacio={estacio} parameterName="fxHigh" top="35%" left="60.9%" />
-            <GrufOnOffButton estacio={estacio} parameterName="fxDelayWet" top="20%" left="81.7%" valueOn={0.5} valueOff={0.0} />
-            <GrufLabel text="Durada" top="29.3%" left="70.3%" />
-            <GrufLabel text="Delay" top="14%" left="82.3%" />
-            <GrufKnobPetit estacio={estacio} parameterName="fxReverbWet" top="7.1%" left="70%" />
+            
             <GrufSliderVertical estacio={estacio} parameterName="hpf" top="8.5%" left="38.2%" width="225px" />
             <GrufSliderVertical estacio={estacio} parameterName="lpf" top="8.5%" left="43%" width="225px" />
             <GrufLabel text="HPF" top="21.8%" left="38.2%" />
             <GrufLabel text="LPF" top="21.8%" left="43%" />
-            <GrufSliderVertical estacio={estacio} parameterName="fxDelayWet" top="29%" left="83%" width="225px" />
-            <GrufSliderVertical estacio={estacio} parameterName="fxDelayFeedback" top="29%" left="89.5%" width="225px" />
-            <GrufLabel text="Mix" top="40.3%" left="83%" />
-            <GrufLabel text="Feedback" top="40.3%" left="87.3%" />
+
+            <GrufLabel text="Delay" top="14%" left="82.3%" />
+            <GrufOnOffButton estacio={estacio} parameterName="fxDelayOnOff" top="20%" left="81.7%" valueOn={0.5} valueOff={0.0} />
+            <GrufLabel text="Durada" top="29.6%" left="70.3%" />
+            <GrufEnum2Columns estacio={estacio} parameterName="fxDelayTime" top="34.2%" left="69.4%" />
+            
+            <GrufSliderVertical estacio={estacio} parameterName="fxDelayWet" top="28%" left="83.6%" labelBottom="0%" labelTop="100%" /> 
+            <GrufSliderVertical estacio={estacio} parameterName="fxDelayFeedback" top="28%" left="88.9%" labelBottom="0%" labelTop="100%" /> 
+            <GrufLabelPetit text="Mix" top="40.3%" left="84%" />
+            <GrufLabelPetit text="Feedback" top="40.3%" left="87.6%" />
+            
             <GrufPianoRoll estacio={estacio} parameterName="notes" top="325px" left="35px" width="750px" height="350px" colorNotes="#d98adc"/>
         </div>
     </div>)
