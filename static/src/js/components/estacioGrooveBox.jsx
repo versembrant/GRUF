88db6c9f0be67465c8d6f747d284d4cf16ff3079
Gonzalo Bañeres <gonzalo.baneres@gmail.com>
2024-09-04 17:16:11 +0200

    Corregeix nom d'alguns paràmetres

--- static/src/js/components/estacioGrooveBox.jsx
+++ static/src/js/components/estacioGrooveBox.jsx
@@ -35,37 +35,37 @@ export const EstacioGrooveBoxUI = ({estacio, setEstacioSelected}) => {
             <GrufLabelPetit text="Mix" top="40.3%" left="84%" />
             <GrufLabelPetit text="Feedback" top="40.3%" left="87.6%" />
            
-            <GrufKnobPetit estacio={estacio} parameterName="volume4" top="48.5%" left="5%" label = 'Vol' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone4" top="48.5%" left="9%" label = 'Tone' />
+            <GrufKnobPetit estacio={estacio} parameterName="volume1" top="48.5%" left="5%" label = 'Vol' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone1" top="48.5%" left="9%" label = 'Tone' />
 
-            <GrufKnobPetit estacio={estacio} parameterName="volume3" top="61.0%" left="5%" label = 'Vol' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone3" top="61.0%" left="9%" label = 'Tone' />
+            <GrufKnobPetit estacio={estacio} parameterName="volume2" top="61.0%" left="5%" label = 'Vol' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone2" top="61.0%" left="9%" label = 'Tone' />
 
-            <GrufKnobPetit estacio={estacio} parameterName="volume2" top="72.5%" left="5%" label = 'Vol' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone2" top="72.5%" left="9%" label = 'Tone' />
+            <GrufKnobPetit estacio={estacio} parameterName="volume3" top="72.5%" left="5%" label = 'Vol' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone3" top="72.5%" left="9%" label = 'Tone' />
 
-            <GrufKnobPetit estacio={estacio} parameterName="volume1" top="85.0%" left="5%" label = 'Vol' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone1" top="85.0%" left="9%" label = 'Tone' />
+            <GrufKnobPetit estacio={estacio} parameterName="volume4" top="85.0%" left="5%" label = 'Vol' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="tone4" top="85.0%" left="9%" label = 'Tone' />
 
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack4" top="48.5%" left="77%" label = 'Attack' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="release4" top="48.5%" left="82%" label = 'Release' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend4" top="48.5%" left="87.5%" label = 'Reverb' />
-            <GrufKnobPetit estacio={estacio} parameterName="swing4" top="48.5%" left="93%" label = 'Swing' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack1" top="48.5%" left="77%" label = 'Attack' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="release1" top="48.5%" left="82%" label = 'Release' />
+            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="48.5%" left="87.5%" label = 'Reverb' />
+            <GrufKnobPetit estacio={estacio} parameterName="swing1" top="48.5%" left="93%" label = 'Swing' />
 
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack3" top="61%" left="77%" label = 'Attack' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="release3" top="61%" left="82%" label = 'Release' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend3" top="61%" left="87.5%" label = 'Reverb' />
-            <GrufKnobPetit estacio={estacio} parameterName="swing3" top="61%" left="93%" label = 'Swing' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack2" top="61%" left="77%" label = 'Attack' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="release2" top="61%" left="82%" label = 'Release' />
+            <GrufKnobPetit estacio={estacio} parameterName="reverbSend2" top="61%" left="87.5%" label = 'Reverb' />
+            <GrufKnobPetit estacio={estacio} parameterName="swing2" top="61%" left="93%" label = 'Swing' />
 
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack2" top="72.5%" left="77%" label = 'Attack' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="release2" top="72.5%" left="82%" label = 'Release' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend2" top="72.5%" left="87.5%" label = 'Reverb' />
-            <GrufKnobPetit estacio={estacio} parameterName="swing2" top="72.5%" left="93%" label = 'Swing' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack3" top="72.5%" left="77%" label = 'Attack' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="release3" top="72.5%" left="82%" label = 'Release' />
+            <GrufKnobPetit estacio={estacio} parameterName="reverbSend3" top="72.5%" left="87.5%" label = 'Reverb' />
+            <GrufKnobPetit estacio={estacio} parameterName="swing3" top="72.5%" left="93%" label = 'Swing' />
 
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack1" top="85%" left="77%" label = 'Attack' />
-            <GrufKnobPetitDiscret estacio={estacio} parameterName="release1" top="85%" left="82%" label = 'Release' />
-            <GrufKnobPetit estacio={estacio} parameterName="reverbSend1" top="85%" left="87.5%" label = 'Reverb' />
-            <GrufKnobPetit estacio={estacio} parameterName="swing1" top="85%" left="93%" label = 'Swing' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="atack4" top="85%" left="77%" label = 'Attack' />
+            <GrufKnobPetitDiscret estacio={estacio} parameterName="release4" top="85%" left="82%" label = 'Release' />
+            <GrufKnobPetit estacio={estacio} parameterName="reverbSend4" top="85%" left="87.5%" label = 'Reverb' />
+            <GrufKnobPetit estacio={estacio} parameterName="swing4" top="85%" left="93%" label = 'Swing' />
 
             <GrufLabelPetitVertical text="OpHat" top="50.7%" left="13.6%" />
             <GrufLabelPetitVertical text="CHat" top="63.3%" left="14%" />
