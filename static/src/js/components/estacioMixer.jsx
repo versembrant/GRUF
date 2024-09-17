746243e8dee4894d932186758a033dd0099b92b5
Gonzalo Bañeres <gonzalo.baneres@gmail.com>
2024-09-17 10:07:53 +0200

    Retorna widgets específics de mixer a estacioMixer i aplica canvis d'estil

--- static/src/js/components/estacioMixer.jsx
+++ static/src/js/components/estacioMixer.jsx
@@ -3,9 +3,110 @@ import { getAudioGraphInstance } from "../audioEngine";
 import { getCurrentSession } from "../sessionManager";
 import { subscribeToStoreChanges} from "../utils";
 import { AudioEffectsControlPanel} from  "./fxControlPanel"
-import { GrufButtonNoBorder, GrufMuteCheckbox, GrufSoloCheckbox, GrufGainSliderVertical, GrufSliderVertical, GrufLabelPetit, GrufLabelEstacio } from "../components/widgets";
+import { GrufButtonNoBorder, GrufLabelEstacio } from "../components/widgets";
 import Checkbox from '@mui/material/Checkbox';
+import Slider from '@mui/material/Slider';
 
+
+export const GrufMuteCheckbox = ({ estacio }) => {
+    const parameterValue = getCurrentSession().getLiveMutesEstacions()[estacio.nom];
+
+    const handleMuteToggle = (evt) => {
+        const currentMutes = getCurrentSession().getLiveMutesEstacions();
+        currentMutes[estacio.nom] = evt.target.checked;
+        getCurrentSession().liveSetMutesEstacions(currentMutes);
+    };
+
+    return (
+        <label className="gruf-mute-checkbox">
+            <input
+                type="checkbox"
+                checked={parameterValue}
+                onChange={handleMuteToggle}
+                className="gruf-mute-checkbox__input" 
+            />
+            <span className="gruf-mute-checkbox__visual">M</span> 
+        </label>
+    );
+};
+
+export const GrufSoloCheckbox = ({ estacio }) => {
+    const parameterValue = getCurrentSession().getLiveSolosEstacions()[estacio.nom];
+
+    const handleSoloToggle = (evt) => {
+        const currentSolos = getCurrentSession().getLiveSolosEstacions();
+        currentSolos[estacio.nom] = evt.target.checked;
+        getCurrentSession().liveSetSolosEstacions(currentSolos);
+    };
+
+    return (
+        <label className="gruf-solo-checkbox">
+            <input
+                type="checkbox"
+                checked={parameterValue}
+                onChange={handleSoloToggle}
+                className="gruf-solo-checkbox__input" 
+            />
+            <span className="gruf-solo-checkbox__visual">S</span> 
+        </label>
+    );
+};
+
+export const GrufGainSlider = ({ estacio }) => {
+    const parameterValue=getCurrentSession().getLiveSolosEstacions()[estacio.nom];
+    return (<div>
+        <input 
+            type="range"
+            min="0.0" 
+            max="1.0"
+            step="0.1"
+            value={getCurrentSession().getLiveGainsEstacions()[estacio.nom]}
+            name={estacio.nom}
+            onInput={(evt) => {
+                const currentGains = getCurrentSession().getLiveGainsEstacions();
+                currentGains[estacio.nom] = parseFloat(evt.target.value, 10);
+                getCurrentSession().liveSetGainsEstacions(currentGains);
+            }}
+        />{estacio.nom}
+    </div>)
+}
+
+export const GrufGainSliderVertical = ({ estacio, top, left, height, fons }) => {
+    const nomEstacio = estacio.nom; 
+    const parameterValue = getCurrentSession().getLiveGainsEstacions()[nomEstacio]; 
+    const marks = [];
+
+    const style = { top: top, left: left };
+    if (height !== undefined) {
+        style.height = height;
+    }
+
+    let classeFons = "";
+    if (fons === "linies") {
+        classeFons = "gruf-slider-background-ratllat";
+    }
+
+    const handleGainChange = (evt, value) => {
+        const currentGains = getCurrentSession().getLiveGainsEstacions();
+        currentGains[nomEstacio] = parseFloat(value, 10);
+        getCurrentSession().liveSetGainsEstacions(currentGains);
+    };
+
+    return (
+        <div className={"gruf-gain-slider-vertical " + classeFons} style={style}>
+            <Slider
+                orientation="vertical"
+                value={parameterValue}
+                step={0.01}
+                min={0.0}
+                max={1.0}
+                marks={marks}
+                onChange={handleGainChange}
+                labelBottom = {nomEstacio}
+            />
+        </div>
+    );
+};
 
 export const EstacioMixerUI = ({setEstacioSelected, showLevelMeters}) => {
     subscribeToStoreChanges(getAudioGraphInstance());
@@ -27,7 +128,6 @@ export const EstacioMixerUI = ({setEstacioSelected, showLevelMeters}) => {
             }
         }
     });
-
 
 
     return (<div key="mixer1" className="estacio estacio-mixer" id="mixerObject">
--- static/src/js/components/widgets.jsx
+++ static/src/js/components/widgets.jsx
@@ -332,61 +332,6 @@ export const GrufSliderVertical = ({ estacio, parameterName, top, left, height, 
     )
 };
 
-export const GrufGainSlider = ({ estacio }) => {
-    const parameterValue=getCurrentSession().getLiveSolosEstacions()[estacio.nom];
-    return (<div>
-        <input 
-            type="range"
-            min="0.0" 
-            max="1.0"
-            step="0.1"
-            value={getCurrentSession().getLiveGainsEstacions()[estacio.nom]}
-            name={estacio.nom}
-            onInput={(evt) => {
-                const currentGains = getCurrentSession().getLiveGainsEstacions();
-                currentGains[estacio.nom] = parseFloat(evt.target.value, 10);
-                getCurrentSession().liveSetGainsEstacions(currentGains);
-            }}
-        />{estacio.nom}
-    </div>)
-}
-
-export const GrufGainSliderVertical = ({ estacio, top, left, height, fons }) => {
-    const nomEstacio = estacio.nom; 
-    const parameterValue = getCurrentSession().getLiveGainsEstacions()[nomEstacio]; 
-    const marks = [];
-
-    const style = { top: top, left: left };
-    if (height !== undefined) {
-        style.height = height;
-    }
-
-    let classeFons = "";
-    if (fons === "linies") {
-        classeFons = "gruf-slider-background-ratllat";
-    }
-
-    const handleGainChange = (evt, value) => {
-        const currentGains = getCurrentSession().getLiveGainsEstacions();
-        currentGains[nomEstacio] = parseFloat(value, 10);
-        getCurrentSession().liveSetGainsEstacions(currentGains);
-    };
-
-    return (
-        <div className={"gruf-gain-slider-vertical " + classeFons} style={style}>
-            <Slider
-                orientation="vertical"
-                value={parameterValue}
-                step={0.01}
-                min={0.0}
-                max={1.0}
-                marks={marks}
-                onChange={handleGainChange}
-                labelBottom = {nomEstacio}
-            />
-        </div>
-    );
-};
 
 export const GrufBpmCounter = ({ top, left }) => {
     const currentBpm = parseInt(getAudioGraphInstance().getBpm(), 10);
@@ -720,48 +665,6 @@ export const GrufSelectorPresets = ({estacio, top, left, height="30px"}) => {
     )
 }
 
-export const GrufMuteCheckbox = ({ estacio }) => {
-    const parameterValue = getCurrentSession().getLiveMutesEstacions()[estacio.nom];
 
-    const handleMuteToggle = (evt) => {
-        const currentMutes = getCurrentSession().getLiveMutesEstacions();
-        currentMutes[estacio.nom] = evt.target.checked;
-        getCurrentSession().liveSetMutesEstacions(currentMutes);
-    };
-
-    return (
-        <label className="gruf-mute-checkbox">
-            <input
-                type="checkbox"
-                checked={parameterValue}
-                onChange={handleMuteToggle}
-                className="gruf-mute-checkbox__input" 
-            />
-            <span className="gruf-mute-checkbox__visual">M</span> 
-        </label>
-    );
-};
-
-export const GrufSoloCheckbox = ({ estacio }) => {
-    const parameterValue = getCurrentSession().getLiveSolosEstacions()[estacio.nom];
-
-    const handleSoloToggle = (evt) => {
-        const currentSolos = getCurrentSession().getLiveSolosEstacions();
-        currentSolos[estacio.nom] = evt.target.checked;
-        getCurrentSession().liveSetSolosEstacions(currentSolos);
-    };
-
-    return (
-        <label className="gruf-solo-checkbox">
-            <input
-                type="checkbox"
-                checked={parameterValue}
-                onChange={handleSoloToggle}
-                className="gruf-solo-checkbox__input" 
-            />
-            <span className="gruf-solo-checkbox__visual">S</span> 
-        </label>
-    );
-};
 
 
