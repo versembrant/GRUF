e783b1b1c5012924ec7ee43b6cfb75b334c77ea1
ffont <frederic.font@gmail.com>
2024-10-01 00:52:08 +0200

    Throttle knobs and sliders to avoid many react re-renders

--- static/src/js/sessionManager.js
+++ static/src/js/sessionManager.js
@@ -349,6 +349,7 @@ export class Session {
         this.useAudioEngine = true
         this.localMode = local
         this.performLocalUpdatesBeforeServerUpdates = true
+        this.continuousControlThrottleTime = 100
         
         // Copia totes les dades "raw" de la sessió per tenir-les guardades
         this.rawData = data
