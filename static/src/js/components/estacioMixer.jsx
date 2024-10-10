dd811f0412809e2dc1f66663317bbbc49d202c05
judit <juditcasalsvila@gmail.com>
2024-10-10 09:44:34 +0200

    Posiciona master track a la dreta

--- static/src/js/components/estacioMixer.jsx
+++ static/src/js/components/estacioMixer.jsx
@@ -352,13 +352,15 @@ export const EstacioMixerUI = ({ setEstacioSelected, showLevelMeters }) => {
                             />
                         );
                     })}
-                    <div className="estacio-mixer-master-columna">
-                        <GrufMasterPanKnob/>
-                        <div className="slider-wrapper">
-                        <GrufMasterGainSliderVertical top='500px' left='50px' height='400px'/>
-                        <GrufMasterMeter showLevelMeters={showLevelMeters} />
+                    <div className="mixer-master-container">
+                        <div className="estacio-mixer-master-columna">
+                            <GrufMasterPanKnob/>
+                            <div className="slider-wrapper">
+                            <GrufMasterGainSliderVertical top='500px' left='50px' height='400px'/>
+                            <GrufMasterMeter showLevelMeters={showLevelMeters} />
+                            </div>
+                            <div className="master-label">Master</div>
                         </div>
-                        <div className="master-label">Master</div>
                     </div>
                 </div>
             </div>
