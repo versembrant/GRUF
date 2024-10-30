9987b46f0fef3f03e20b3bc8f4dc488ba9c13770
Asmatzaile <gorkainventor@gmail.com>
2024-10-30 17:28:09 +0100

    approximate sampler upper half styles to new design

--- static/src/js/components/estacioSampler.jsx
+++ static/src/js/components/estacioSampler.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { GrufPianoRoll, GrufKnob, GrufButtonNoBorder, GrufPadGrid, GrufSlider, GrufSelectorSonsSampler, GrufSelectorLoopMode} from "./widgets";
 import { GrufModulADSR, GrufModulEQ, GrufModulDelay, GrufModulReverb } from "./moduls";
 import { EntradaMidiTeclatQUERTYHidden } from "./entradaMidi";
-import { AudioRecorder } from "../components/audioRecorder";
 
 export const EstacioSamplerUI = ({estacio, setEstacioSelected}) => {
 
@@ -18,40 +17,40 @@ export const EstacioSamplerUI = ({estacio, setEstacioSelected}) => {
                 <EntradaMidiTeclatQUERTYHidden estacio={estacio} />
                 
                 <GrufButtonNoBorder text="Canvia estació" top="44px" left="826px" onClick={() => {setEstacioSelected(undefined)}} />
-
-                <GrufSlider orientation="vertical" estacio={estacio} parameterName="hpf" labelSize="14px" top="8.5%" left="29%" size="150px" fons="linies" />
-                <GrufSlider orientation="vertical" estacio={estacio} parameterName="lpf" labelSize="14px" top="8.5%" left="33.8%" size="150px" fons="linies" />
                 
+                <div className="modul-border modul-bg" style={{position: "absolute", top: "4.7%", left: "3.6%", width: 220}}>
+                    <div className="flex justify-between" >
+                        <GrufKnob mida="gran" parameterParent={estacio} parameterName='gain' label='Vol' />
+                        <GrufKnob mida="gran" parameterParent={estacio} parameterName='cutoff' label='Cutoff' />
+                    </div>
+                    <GrufSelectorSonsSampler estacio={estacio} parameterName={`sound`} />  {/* sound${selectedPad + 1} if we wanted to control them individually, also need to change estacioSampler.js*/}
+                </div>
+
+                <div className="modul-border modul-bg flex flex-col gap-10" style={{position: "absolute", top: "25.7%", left: "3.6%", width: 220}}>
+                    <div className="flex justify-between">
+                        <GrufKnob mida="petit" parameterParent={estacio} parameterName={`start${selectedPad + 1}`} label='Start' />
+                        <GrufKnob mida="petit" parameterParent={estacio} parameterName={`end${selectedPad + 1}`} label='End' />
+                    </div>
+                    <div className="flex justify-between">
+                        {/* <GrufKnob mida="petit" parameterParent={estacio} parameterName={`volume${selectedPad + 1}`} label='Vol' /> */}
+                        <GrufKnob mida="petit" parameterParent={estacio} parameterName={`pan${selectedPad + 1}`} label='Pan' />
+                        <GrufKnob mida="petit" parameterParent={estacio} parameterName={`pitch${selectedPad + 1}`} label='Pitch' />
+                        <GrufSelectorLoopMode estacio={estacio} parameterName={`loopMode${selectedPad + 1}`} />
+                    </div>
+                </div>
+
+                <GrufModulADSR estacio={estacio} soundNumber={selectedPad + 1} top="4.8%" left="26.8%" height="276px"/>
+
                 <GrufModulEQ estacio={estacio} top="25.4%" left="49.5%"/>
                 <GrufModulDelay estacio={estacio} top="11.5%" left="80.1%"/>
                 <GrufModulReverb estacio={estacio} top="4.8%" left="49.4%" />
-
-                <GrufKnob mida="gran" parameterParent={estacio} parameterName={`volume${selectedPad + 1}`} top="50%" left="5%" label='Vol' />
-                <GrufKnob mida="gran" parameterParent={estacio} parameterName={`pan${selectedPad + 1}`} top="64.5%" left="5%" label='Pan' />
-                <GrufKnob mida="gran" parameterParent={estacio} parameterName={`pitch${selectedPad + 1}`} top="79%" left="5%" label='Pitch' />
-
-                <GrufModulADSR estacio={estacio} soundNumber={selectedPad + 1} top="4.8%" left="3.7%" height="197px"/>
-
-                <GrufKnob mida="gran" parameterParent={estacio} parameterName={`start${selectedPad + 1}`} top="7.3%" left="41.3%" label='Start' />
-                <GrufKnob mida="gran" parameterParent={estacio} parameterName={`end${selectedPad + 1}`} top="21%" left="41.3%" label='End' />
-
-                <GrufSelectorLoopMode estacio={estacio} parameterName={`loopMode${selectedPad + 1}`} />
-
-                 <GrufSelectorSonsSampler estacio={estacio} parameterName={`sound`} top="268px" left="60px" width="220" />  {/* sound${selectedPad + 1} if we wanted to control them individually, also need to change estacioSampler.js*/}
 
                 <div className="flex gap-10" style={{position:"absolute", top:"327px", left:"38px"}}>
                     <GrufPadGrid estacio={estacio}  width="250px" height="333px" onPadClick={handlePadClick} currentSelectedPad={selectedPad} />
                     <GrufPianoRoll estacio={estacio} parameterName="notes" width="550px" height="335px" colorNotes="#00e2d3" modeSampler="true"/>
                 </div>
                 
-                <div style={{position:"absolute", top:"263px", left:"295px"}}>
-                    <AudioRecorder ui="minimal" onRecordUploadedCallback={(data) => {
-                        console.log("Sound uploaded to server: ", data.url);
-                        estacio.updateParametreEstacio('selectedSoundName', data.url.split("/").slice(-1)[0])
-                    }} />
-                </div>
             </div>
-
         </div>
     )
 };
--- static/src/js/components/moduls.jsx
+++ static/src/js/components/moduls.jsx
@@ -1,7 +1,6 @@
 import { GrufKnob, GrufSlider, GrufToggle, GrufLegend, GrufEnum2Columns, GrufReverbTime, ADSRGraph } from "./widgets";
 
 
-// TODO: position: relative should be default for knobs
 export const GrufModulADSR = ({estacio, soundNumber="", height, top, left}) => {
     const attackParamName = `attack${soundNumber}`;
     const decayParamName = `decay${soundNumber}`;
@@ -12,10 +11,10 @@ export const GrufModulADSR = ({estacio, soundNumber="", height, top, left}) => {
         <div className="gruf-adsr-widget" style={{top, left, height}}>
             <ADSRGraph estacio={estacio} adsrParameterNames={[attackParamName, decayParamName, sustainParamName, releaseParamName]}/>
             <div className="adsr-knobs">
-                <GrufKnob mida="petit" parameterParent={estacio} parameterName={attackParamName} position='relative' label='Attack'/>
-                <GrufKnob mida="petit" parameterParent={estacio} parameterName={decayParamName} position='relative' label='Decay'/>
-                <GrufKnob mida="petit" parameterParent={estacio} parameterName={sustainParamName} position='relative' label='Sustain'/>
-                <GrufKnob mida="petit" parameterParent={estacio} parameterName={releaseParamName} position='relative' label='Release'/>
+                <GrufKnob mida="petit" parameterParent={estacio} parameterName={attackParamName} label='Attack'/>
+                <GrufKnob mida="petit" parameterParent={estacio} parameterName={decayParamName} label='Decay'/>
+                <GrufKnob mida="petit" parameterParent={estacio} parameterName={sustainParamName} label='Sustain'/>
+                <GrufKnob mida="petit" parameterParent={estacio} parameterName={releaseParamName} label='Release'/>
             </div>
         </div>
     )
@@ -27,9 +26,9 @@ export const GrufModulEQ = ({estacio, top, left}) => {
             <GrufLegend text="EQ" />
             <GrufToggle estacio={estacio} parameterName="fxEqOnOff" />
             <div>
-                <GrufKnob mida="gran" customWidth="50px" customHeight="50px" parameterParent={estacio} parameterName="fxLow" position='relative'/>
-                <GrufKnob mida="gran" customWidth="50px" customHeight="50px" parameterParent={estacio} parameterName="fxMid" position='relative' />
-                <GrufKnob mida="gran" customWidth="50px" customHeight="50px" parameterParent={estacio} parameterName="fxHigh" position='relative' />
+                <GrufKnob mida="gran" customWidth="50px" customHeight="50px" parameterParent={estacio} parameterName="fxLow"/>
+                <GrufKnob mida="gran" customWidth="50px" customHeight="50px" parameterParent={estacio} parameterName="fxMid" />
+                <GrufKnob mida="gran" customWidth="50px" customHeight="50px" parameterParent={estacio} parameterName="fxHigh" />
             </div>
         </fieldset>
     )
@@ -41,8 +40,8 @@ export const GrufModulDelay = ({estacio, top, left}) => {
         <fieldset className="gruf-modul gruf-modul-delay" style={{position: "absolute", top, left}}>
             <GrufLegend text="Delay" />
             <div>
-                <GrufKnob mida="petit" parameterParent={estacio} parameterName="fxDelayWet" label="Send" position="relative" /> 
-                <GrufKnob mida="petit" parameterParent={estacio} parameterName="fxDelayFeedback" label="Feedback" position="relative" /> 
+                <GrufKnob mida="petit" parameterParent={estacio} parameterName="fxDelayWet" label="Send" /> 
+                <GrufKnob mida="petit" parameterParent={estacio} parameterName="fxDelayFeedback" label="Feedback" /> 
             </div>
             <fieldset>
                 <GrufLegend text="Durada" />
@@ -56,7 +55,7 @@ export const GrufModulReverb = ({estacio, top, left}) => {
     return(
         <fieldset className="gruf-modul gruf-modul-reverb" style={{position: "absolute", top, left}}>
             <GrufLegend text="Reverb" />
-            <GrufKnob mida="gran" parameterParent={estacio} parameterName="fxReverbWet" label="Send" />
+            <GrufKnob mida="gran" parameterParent={estacio} parameterName="fxReverbWet" position="absolute" label="Send" />
             <fieldset>
                 <GrufLegend text="Durada" bare="true"></GrufLegend>
                 <GrufReverbTime estacio={estacio} parameterName="fxReverbDecay" />
