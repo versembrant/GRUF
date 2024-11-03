accf0a32ae5c4caa75b49e360f433677a09a519a
Asmatzaile <gorkainventor@gmail.com>
2024-11-03 13:03:42 +0100

    nou disseny a synth de can passavia

--- static/src/js/components/estacioSynth.jsx
+++ static/src/js/components/estacioSynth.jsx
@@ -1,4 +1,4 @@
-import { GrufKnob, GrufButtonNoBorder, GrufSlider, GrufPianoRoll, GrufSelectorTonalitat } from "./widgets";
+import { GrufKnob, GrufSeparatorLine, GrufButtonNoBorder, GrufSlider, GrufPianoRoll, GrufSelectorTonalitat } from "./widgets";
 import { GrufModulADSR, GrufModulEQ, GrufModulDelay, GrufModulReverb } from "./moduls";
 import { EntradaMidiTeclatQUERTYHidden } from "./entradaMidi";
 
@@ -14,25 +14,37 @@ export const EstacioSynthBaseUI = ({estacio, setEstacioSelected, colorNotesPiano
     return (
         <div className="estacio-main">
             <EntradaMidiTeclatQUERTYHidden estacio={estacio} />
-            
             <GrufButtonNoBorder text="Canvia estació" top="44px" left="826px" onClick={() => {setEstacioSelected(undefined)}} />
-            <GrufKnob mida="gran" parameterParent={estacio} parameterName="gain" top="80%" left="85%" label="Vol" />
-            <GrufModulADSR estacio={estacio} top="4.8%" left="3.7%" height="276px"/>
-            <GrufKnob mida="gran" parameterParent={estacio} parameterName="fxDrive" top="8%" left="28%" />
-            <GrufKnob mida="gran" parameterParent={estacio} parameterName="harmonicity" top="30%" left="28%" />
+
+            <fieldset className="modul-border modul-bg flex flex-col justify-between" style={{position: "absolute", top: "4.7%", left: "3.6%", width: 220}}>
+                <fieldset className="flex justify-between">
+                    <GrufKnob mida="gran" parameterParent={estacio} parameterName="gain" label="Vol" />
+                    <GrufSeparatorLine />
+                    <GrufKnob mida="gran" parameterParent={estacio} parameterName="fxDrive" />
+                </fieldset>
+            <GrufSelectorTonalitat label={null}/>
+            </fieldset>
+
+            <fieldset className="modul-border modul-bg flex justify-between items-center" style={{position: "absolute", top: "28.2%", left: "3.6%", width: 220}}>
+                <fieldset className="flex justify-between items-center">
+                    <GrufSlider orientation="vertical" estacio={estacio} parameterName="waveform" size="56px" noLabel="true" noOutput="true"/>
+                    <GrufKnob mida="gran" parameterParent={estacio} parameterName="harmonicity" />
+                </fieldset>
+                <GrufSeparatorLine />
+                <fieldset className="flex flex-col justify-between items-center">
+                    <GrufKnob mida="petit" parameterParent={estacio} parameterName="hpf" />
+                    <GrufKnob mida="petit" parameterParent={estacio} parameterName="lpf" />
+                </fieldset>
+            </fieldset>
             
-            <GrufSlider orientation="vertical" estacio={estacio} parameterName="hpf" labelSize="14px" top="8.5%" left="38.2%" size="100px" />
-            <GrufSlider orientation="vertical" estacio={estacio} parameterName="lpf" labelSize="14px" top="8.5%" left="43%" size="100px"/>
-            
+            <GrufModulADSR estacio={estacio} top="4.8%" left="26.8%" height="276px"/>
+
             <GrufModulEQ estacio={estacio} top="25.4%" left="49.5%"/>
             <GrufModulDelay estacio={estacio} top="11.5%" left="80.1%"/>
             <GrufModulReverb estacio={estacio} top="4.8%" left="49.4%" />
 
-            <GrufSelectorTonalitat position="absolute" top="50.3%" left="81.4%" />
-       
-            <GrufSlider orientation="vertical" estacio={estacio} parameterName="waveform" top="30%" left="36.5%" size="56px" noLabel="true" noOutput="true"/>
-    
+
             <GrufPianoRoll estacio={estacio} parameterName="notes" top="325px" left="35px" width="750px" height="343px" colorNotes={colorNotesPiano} />
         </div>
     )
-}
+};
