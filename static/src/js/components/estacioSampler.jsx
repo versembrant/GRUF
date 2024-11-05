537c27dc7df145c77a07c18b2d69480fb9adfa8f
Asmatzaile <gorkainventor@gmail.com>
2024-11-05 12:14:38 +0100

    gridificar sampler

--- static/src/js/components/estacioSampler.jsx
+++ static/src/js/components/estacioSampler.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { GrufPianoRoll, GrufKnob, GrufButtonNoBorder, GrufPadGrid, GrufSlider, GrufSelectorSonsSampler, GrufSelectorLoopMode} from "./widgets";
+import { GrufPianoRoll, GrufKnob, GrufButtonBorder, GrufPadGrid, GrufSlider, GrufSelectorSonsSampler, GrufSelectorLoopMode, GrufSeparatorLine, GrufNoteControls} from "./widgets";
 import { GrufModulADSR, GrufModulEQ, GrufModulDelay, GrufModulReverb } from "./moduls";
 import { EntradaMidiTeclatQUERTYHidden } from "./entradaMidi";
 
@@ -13,45 +13,46 @@ export const EstacioSamplerUI = ({estacio, setEstacioSelected}) => {
 
     return (
         <div key={estacio.nom} className="estacio estacio-sampler">
-            <div className="estacio-main">
+            <div className="estacio-main grid gap-10 p-4">
                 <EntradaMidiTeclatQUERTYHidden estacio={estacio} />
+                <GrufButtonBorder className="col-start-4 row-start-1" text="Canvia estació" onClick={() => {setEstacioSelected(undefined)}} />
                 
-                <GrufButtonNoBorder text="Canvia estació" top="44px" left="826px" onClick={() => {setEstacioSelected(undefined)}} />
-                
-                <div className="modul-border modul-bg" style={{position: "absolute", top: "4.7%", left: "3.6%", width: 220}}>
+                <fieldset className="modul-border modul-bg flex flex-col gap-10 justify-between col-start-1 row-start-1 row-span-2">
                     <div className="flex justify-between" >
                         <GrufKnob mida="gran" parameterParent={estacio} parameterName='gain' label='Vol' />
+                        <GrufSeparatorLine/>
                         <GrufKnob mida="gran" parameterParent={estacio} parameterName='cutoff' label='Cutoff' />
                     </div>
-                    <GrufSelectorSonsSampler estacio={estacio} parameterName={`sound`} />  {/* sound${selectedPad + 1} if we wanted to control them individually, also need to change estacioSampler.js*/}
-                </div>
+                    <GrufSelectorSonsSampler estacio={estacio} parameterName={`sound`} width='180px'/>  {/* sound${selectedPad + 1} if we wanted to control them individually, also need to change estacioSampler.js*/}
+                </fieldset>
 
-                <div className="modul-border modul-bg flex flex-col gap-10" style={{position: "absolute", top: "25.7%", left: "3.6%", width: 220}}>
-                    <div>
+                <fieldset className="modul-border modul-bg flex flex-col gap-10 col-start-1 row-start-3">
+                    <fieldset>
                         <GrufSlider estacio={estacio} parameterName={[`start${selectedPad + 1}`, `end${selectedPad + 1}`]} noLabel noOutput />
-                        <div className="flex justify-between text-12" aria-hidden>
+                        <fieldset className="flex justify-between text-12" aria-hidden>
                             <span>Start</span>
                             <span>End</span>
-                        </div>
-                    </div>
-                    <div className="flex justify-between">
+                        </fieldset>
+                    </fieldset>
+                    <fieldset className="flex justify-between">
                         {/* <GrufKnob mida="petit" parameterParent={estacio} parameterName={`volume${selectedPad + 1}`} label='Vol' /> */}
                         <GrufKnob mida="petit" parameterParent={estacio} parameterName={`pan${selectedPad + 1}`} label='Pan' />
                         <GrufKnob mida="petit" parameterParent={estacio} parameterName={`pitch${selectedPad + 1}`} label='Pitch' />
                         <GrufSelectorLoopMode estacio={estacio} parameterName={`loopMode${selectedPad + 1}`} />
-                    </div>
-                </div>
+                    </fieldset>
+                </fieldset>
 
-                <GrufModulADSR estacio={estacio} soundNumber={selectedPad + 1} top="4.8%" left="26.8%" height="276px"/>
+                <GrufModulADSR className="col-start-2 row-start-1 row-span-3" estacio={estacio} soundNumber={selectedPad + 1}height="276px"/>
 
-                <GrufModulEQ estacio={estacio} top="25.4%" left="49.5%"/>
-                <GrufModulDelay estacio={estacio} top="11.5%" left="80.1%"/>
-                <GrufModulReverb estacio={estacio} top="4.8%" left="49.4%" />
+                <GrufModulEQ className="col-start-3 row-start-3" estacio={estacio}/>
+                <GrufModulDelay className="col-start-4 row-start-2 row-span-2" estacio={estacio}/>
+                <GrufModulReverb className="col-start-3 row-start-1 row-span-2" estacio={estacio}/>
 
-                <div className="flex gap-10" style={{position:"absolute", top:"327px", left:"38px"}}>
+                <fieldset className="flex gap-10 col-start-1 col-span-4 row-start-4">
                     <GrufPadGrid estacio={estacio}  width="250px" height="333px" onPadClick={handlePadClick} currentSelectedPad={selectedPad} />
                     <GrufPianoRoll estacio={estacio} parameterName="notes" width="550px" height="335px" colorNotes="#00e2d3" modeSampler="true"/>
-                </div>
+                    <GrufNoteControls className="modul-border flex flex-col gap-10 justify-between align-center" estacio={estacio} width="100px"/>
+                </fieldset>
                 
             </div>
         </div>
