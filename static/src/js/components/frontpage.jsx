a5644357eecb4b604e8d7f69c716a0aa6169f24c
Asmatzaile <gorkainventor@gmail.com>
2024-11-08 11:59:50 +0100

    fica les seccions del landing content en contenidors separats

--- static/src/js/components/frontpage.jsx
+++ static/src/js/components/frontpage.jsx
@@ -23,54 +23,63 @@ export const Footer = () => {
 const LandingContent = () => {
     return (
         <div id="landingContent">
-            <h2>Què és el GRUF?</h2>
-            <p>
-                <b>El GRUF és un programari lliure de producció musical</b>, en versió web i aplicació, dissenyat especialment per a l’aprenentatge de competències musicals i artístiques, i que integra els principals instruments que s’utilitzen en l’àmbit de la producció musical actual (sàmpler, seqüenciador, sintetitzador, mesclador…). Amb el GRUF podràs aprendre a produir els teus pròpis beats o crear en comunitat de forma col·laborativa. 
-            </p>
-            <div style={{display: "flex", justifyContent:"center", marginTop: "30px", marginBottom: "30px"}}>
-                <div style={{border:"1px solid white", width:"800px", height:"500px"}}>
-                    VIDEO INCRUSTAT
+            <section id="descripcio-curta">
+                <h2>Què és el GRUF?</h2>
+                <p>
+                    <b>El GRUF és un programari lliure de producció musical</b>, en versió web i aplicació, dissenyat especialment per a l’aprenentatge de competències musicals i artístiques, i que integra els principals instruments que s’utilitzen en l’àmbit de la producció musical actual (sàmpler, seqüenciador, sintetitzador, mesclador…). Amb el GRUF podràs aprendre a produir els teus pròpis beats o crear en comunitat de forma col·laborativa.
+                </p>
+                <div style={{display: "flex", justifyContent:"center", marginTop: "30px", marginBottom: "30px"}}>
+                    <div style={{border:"1px solid white", width:"800px", height:"500px"}}>
+                        VIDEO INCRUSTAT
+                    </div>
                 </div>
-            </div>
+            </section>
 
-            <h2>Un projecte de Versembrant</h2>
-            <p>
-                Versembrant SCCL és una escola itinerant constituïda en cooperativa de treball associat que ha viscut un creixement i consolidació notable els 4 darrers anys. 
-            </p>
-            <p>
-                Som un projecte socioeducatiu de formació i acompanyament per a la creació artística,  nascut amb l’objectiu d’impulsar el treball en valors com la igualtat, l’esperit crític, la cooperació, la no discriminació i l’educació emocional entre infants i joves. Ho fem mitjançant activitats educatives emmarcades dins de la cultura urbana -i el Hip Hop en particular- en centres educatius.
-            </p>
+            <section id="projecte-versembrant">
+                <h2>Un projecte de Versembrant</h2>
+                <p>
+                    Versembrant SCCL és una escola itinerant constituïda en cooperativa de treball associat que ha viscut un creixement i consolidació notable els 4 darrers anys.
+                </p>
+                <p>
+                    Som un projecte socioeducatiu de formació i acompanyament per a la creació artística,  nascut amb l’objectiu d’impulsar el treball en valors com la igualtat, l’esperit crític, la cooperació, la no discriminació i l’educació emocional entre infants i joves. Ho fem mitjançant activitats educatives emmarcades dins de la cultura urbana -i el Hip Hop en particular- en centres educatius.
+                </p>
+            </section>
 
-            <h2>Marc pedagòic</h2>
-            <p>
-                El GRUF està dissenyat per ser molt més que un programari lliure de creació de beats. També ofereix la possibilitat de ser integrat dins l’àmbit de l’aprenentatge musical i artístic a l’aula, tant a Primària com a Secundària, com altres centres d'ensenyament musical reglat i no reglat. 
-            </p>
-            <p>
-                Oferim una guia per al professorat amb materials formatius amb l’objectiu no només de capacitar-los en la utilització del programari, sinó que també oferim una sèrie d’unitats didàctiques dins les competències bàsiques dins els currículums.
-            </p>
-            <p style={{textAlign: "center", marginTop: "30px", marginBottom: "30px"}}>
-                <a href="#">Descarrega el manual</a>
-            </p>
-            <p>
-                Vols el material formatiu i la proposta d'unitats didàctiques? <a href="#" className="no-button" >Contacta'ns</a>
-            </p>
+            <section id="marc-pedagogic">
+                <h2>Marc pedagògic</h2>
+                <p>
+                    El GRUF està dissenyat per ser molt més que un programari lliure de creació de beats. També ofereix la possibilitat de ser integrat dins l’àmbit de l’aprenentatge musical i artístic a l’aula, tant a Primària com a Secundària, com altres centres d'ensenyament musical reglat i no reglat.
+                </p>
+                <p>
+                    Oferim una guia per al professorat amb materials formatius amb l’objectiu no només de capacitar-los en la utilització del programari, sinó que també oferim una sèrie d’unitats didàctiques dins les competències bàsiques dins els currículums.
+                </p>
+                <p style={{textAlign: "center", marginTop: "30px", marginBottom: "30px"}}>
+                    <a href="#">Descarrega el manual</a>
+                </p>
+                <p>
+                    Vols el material formatiu i la proposta d'unitats didàctiques? <a href="#" className="no-button" >Contacta'ns</a>
+                </p>
+            </section>
 
-            <h2>Programari lliure</h2>
-            <p>
-                EL GRUF és un programari lliure sota una llicència <a href="https://www.gnu.org/licenses/quick-guide-gplv3.ca.html" className="no-button" target="_blank">GPLv3</a>, això vol dir que el codi font del GRUF és obert i lliure, i que qualsevol persona pot accedir-hi, modificar-lo, compartir-lo i contribuïr en el seu desenvolupament.
+            <section id="programari-lliure">
+                <h2>Programari lliure</h2>
+                <p>
+                    EL GRUF és un programari lliure sota una llicència <a href="https://www.gnu.org/licenses/quick-guide-gplv3.ca.html" className="no-button" target="_blank">GPLv3</a>, això vol dir que el codi font del GRUF és obert i lliure, i que qualsevol persona pot accedir-hi, modificar-lo, compartir-lo i contribuïr en el seu desenvolupament.
+                </p>
+                <p style={{textAlign:"center", marginTop:"30px", marginBottom: "50px"}}>
+                    <a href="https://github.com/versembrant/GRUF">Repositori de codi</a>
+                </p>
+            </section>
 
-            </p>
-            <p style={{textAlign:"center", marginTop:"30px", marginBottom: "50px"}}>
-                <a href="https://github.com/versembrant/GRUF">Repositori de codi</a>
-            </p>
-
-            <h2>Opinions</h2>
-            <p>
-                “El GRUF és una eina molt potent per a l’aprenentatge musical i artístic” - Nom Cognom o Entitat o whatever
-            </p>
-            <p>
-                “El GRUF és una eina molt potent per a l’aprenentatge musical i artístic” - Nom Cognom o Entitat o whatever
-            </p>
+            <section id="opinions">
+                <h2>Opinions</h2>
+                <p>
+                    “El GRUF és una eina molt potent per a l’aprenentatge musical i artístic” - Nom Cognom o Entitat o whatever
+                </p>
+                <p>
+                    “El GRUF és una eina molt potent per a l’aprenentatge musical i artístic” - Nom Cognom o Entitat o whatever
+                </p>
+            </section>
     </div>
     )
 }
