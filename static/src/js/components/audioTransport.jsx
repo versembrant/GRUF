8694e09195a587cfe2dfeba00fc7dc041976a1c0
Asmatzaile <gorkainventor@gmail.com>
2024-11-17 11:46:25 +0100

    combina botons d'audiotransport

--- static/src/js/components/audioTransport.jsx
+++ static/src/js/components/audioTransport.jsx
@@ -17,31 +17,18 @@ const handlePlayButton = async (playMode) => {
     }, 100)
 }
 
-export const AudioTransportPlayStop = () => {
+// Aquest play/stop el mostrem a la part superior de les estacions
+export const AudioTransportPlayStop = ({ playMode='live' }) => {
     subscribeToStoreChanges(getAudioGraphInstance());
-    // Aquest play/stop el mostrem a la part superior de les estacions. Només mostra l'estat de "isPlaying" si s'està fent play en mode live, no en mode arranjament
-    const imgSrc = !getAudioGraphInstance().isPlaying() ? icona_play_live :
+    const imgSrc = !getAudioGraphInstance().isPlaying() ? playMode === 'live' ? icona_play_live : icona_play_arranjament :
         getAudioGraphInstance().isPlayingArranjament() ? icona_stop_arranjament : icona_stop_live;
     return (
         <div>
             <button disabled={!getAudioGraphInstance().isGraphBuilt()}
             className="btn-petit"
-            onClick={() => handlePlayButton('live')}>
+            onClick={() => handlePlayButton(playMode)}>
                 <img height="16px" src={imgSrc}/>
             </button>
         </div>
     )
 };
-
-export const PlayArranjamentButton = () => {
-    return(
-        <button disabled={!getAudioGraphInstance().isGraphBuilt()}
-        className="btn-petit"
-        onClick={() => handlePlayButton('arranjament')}>
-        {getAudioGraphInstance().isPlaying() ?
-            <img height="16px" src={getAudioGraphInstance().isPlayingArranjament() ? (appPrefix + "/static/src/img/stop_button_grid.svg"): (appPrefix + "/static/src/img/stop_button.svg")}/>
-        : <img height="16px" src={appPrefix + "/static/src/img/play_button_grid.svg"}/>}
-        </button>
-    )
-}
-
